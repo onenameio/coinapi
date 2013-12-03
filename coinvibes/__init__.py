1f4d9d66ca3c833508b93229542436da73db171b
rxl <me@ryanshea.org>
2013-12-02 19:07:52 -0500

    try fixing ip logging on heroku

--- coinvibes/__init__.py
+++ coinvibes/__init__.py
@@ -11,3 +11,6 @@ import coinvibes.api_v1
 import analytics
 
 analytics.init('5asae1e0a2gpb8uu0sx0')
+
+## use HerokuRequest class so we get real IPs
+app.request_class = utils.HerokuRequest
--- coinvibes/utils.py
+++ coinvibes/utils.py
@@ -35,3 +35,23 @@ def identify_user(f):
         return f(*args, **kwargs)
     return decorated_function
 
+# Heroku Request
+
+from flask import Request
+ 
+class HerokuRequest(Request):
+    """
+    `Request` subclass that overrides `remote_addr` with Heroku's
+    HTTP_X_FORWARDED_FOR when available.
+    """
+ 
+    @property
+    def remote_addr(self):
+        """The remote address of the client."""
+        fwd = self.environ.get('HTTP_X_FORWARDED_FOR', None)
+        if fwd is None:
+            return self.environ.get('REMOTE_ADDR')
+        # sometimes x-forwarded-for contains multiple addresses,
+        # actual client is first, rest are proxy
+        fwd = fwd.split(',')[0]
+        return fwd
